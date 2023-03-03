0fe0d793201298aa6fd23a468f957d7e7a161226
hritikv <hritik5269@gmail.com>
2023-03-03 11:19:08 +0530

    modern bank app design

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from './Billing';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Business from './Business';
+import Button from './Button';
+import Clients from './Clients';
+import CTA from './CTA';
+import FeedbackCard from './FeedbackCard';
+import GetStarted from './GetStarted';
+import Stats from './Stats';
+import Testimonials from './Testimonials';
+import CardDeal from './CardDeal';
+import Hero from './Hero';
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Footer,
+    FeedbackCard,
+    CTA,
+    Button,
+    Clients,
+    GetStarted,
+    Stats,
+    Testimonials,
+    Hero
+};
